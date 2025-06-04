b31b068badbf3ce25e0e805132b9f8a70f5d49ad
Killing Tom <tom.killing@iut-rodez.fr>
2025-06-04 16:26:07 +0200

    application corrigee

--- src/iut/info1/saeihm/ControleurJeu.java
+++ src/iut/info1/saeihm/ControleurJeu.java
@@ -1,6 +1,7 @@
 
 package iut.info1.saeihm;
 
+import iut.info1.saeihm.classes.Plateau;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -33,6 +34,8 @@ public class ControleurJeu {
     private Button btnMenu;
 
     private int joueurActuel = 1;
+    
+    private Plateau plateau = new Plateau();
 
     /**
      * Méthode appelée automatiquement après le chargement du fichier FXML.
@@ -76,13 +79,23 @@ public class ControleurJeu {
     @FXML
     private void clickGrille(MouseEvent event) {
         Node cellule = (Node) event.getSource();
-        if (cellule instanceof Label) {
-            Label labelCellule = (Label) cellule;
-            if (labelCellule.getText() == null || labelCellule.getText().isEmpty()) {
-                labelCellule.setText(joueurActuel == 1 ? "X" : "O");
-                labelCellule.setStyle("-fx-font-size: 24px; -fx-alignment: center;");
+       
+        if (cellule instanceof Button) {
+            Button buttonCellule = (Button) cellule;
+            Integer row = GridPane.getRowIndex(cellule);
+            Integer col = GridPane.getColumnIndex(cellule);
+           
+            
+            if (plateau.getCase(row, col) == 0) { 
+                plateau.setCase(row, col, joueurActuel); 
+                buttonCellule.setText(joueurActuel == 1 ? "X" : "O");
+                buttonCellule.setStyle("-fx-font-size: 12px; -fx-alignment: center;");
+                if (plateau.rechercheSequences(row, col, joueurActuel)) {
+                    comptabiliserPoints(joueurActuel);
+                }            
+             
                 changerJoueur();
-                updateTour();
+                updateTour(); 
             } else {
                 Alert alert = new Alert(Alert.AlertType.WARNING);
                 alert.setTitle("Action invalide");
@@ -92,6 +105,44 @@ public class ControleurJeu {
             }
         }
     }
+    
+    /**
+     * Parcourt tout le tableau et surligne les cases :
+     * - en jaune si elles contiennent 1
+     * - en vert si elles contiennent 2
+     */
+    @FXML
+    private void comptabiliserPoints(int joueur) {
+        int scoreJoueur1 = Integer.parseInt(scoreJ1.getText());
+        int scoreJoueur2 = Integer.parseInt(scoreJ2.getText());
+
+        for (Node cellule : grille.getChildren()) {
+            if (cellule instanceof Button) {
+                Button buttonCellule = (Button) cellule;
+                Integer row = GridPane.getRowIndex(cellule);
+                Integer col = GridPane.getColumnIndex(cellule);
+
+                if (joueur == plateau.getCase(row, col)) {
+                    switch (joueur) {
+                        case 1 -> {
+                            buttonCellule.setStyle("-fx-background-color: yellow;");
+                            scoreJoueur1 += 10;
+                        }
+                        case 2 -> {
+                            buttonCellule.setStyle("-fx-background-color: green;");
+                            scoreJoueur2 += 10;
+                        }
+                        default -> buttonCellule.setStyle("");
+                    }
+                }
+            }
+        }
+
+        // Mise à jour des scores affichés
+        setScoreJ1(scoreJoueur1);
+        setScoreJ2(scoreJoueur2);
+    }
+
 
     /**
      * Met à jour le label du joueur dont c’est le tour.
--- src/iut/info1/saeihm/classes/Plateau.java
+++ src/iut/info1/saeihm/classes/Plateau.java
@@ -1,21 +1,22 @@
+
 package iut.info1.saeihm.classes;
 
 /**
  * Permet la création d'un plateau de jeu des 5 croix avec methodes associées
  */
 public class Plateau {
-    
+
     private static final int TAILLE_TABLEAU = 10;
     private static final int LONGUEUR_LIGNE = 5;
     private int[][] grille;
-    private int[][] grilleSequences;
+    private int[][] grillePrecedente; // Renommé depuis grilleSequences
 
     /**
      * Crées un objet plateau
      */
     public Plateau() {
         this.grille = new int[TAILLE_TABLEAU][TAILLE_TABLEAU];
-        this.grilleSequences = new int[TAILLE_TABLEAU][TAILLE_TABLEAU];
+        this.grillePrecedente = new int[TAILLE_TABLEAU][TAILLE_TABLEAU];
     }
 
     /**
@@ -42,8 +43,8 @@ public class Plateau {
             System.out.println(e.getMessage());
         }
         grille[i][j] = valeur;
-    }   
-    
+    }
+
     /**
      * @return la grille du plateau
      */
@@ -61,59 +62,69 @@ public class Plateau {
             }
         }
     }
-    
+
     /**
      * @param xCase position x de la case sélectionnée
      * @param yCase position y de la case sélectionnée
-     * @param numeroJoueur numéro du joueur pour lequel rechercher les sequences
-     * 
+     * @param numeroJoueur numéro du joueur pour lequel rechercher les séquences
+     * @return true si une séquence a été détectée, false sinon
      */
-    public void rechercheSequences(int xCase, int yCase, int numeroJoueur) {
-        int[][] directions = { 
+
+    public boolean rechercheSequences(int xCase, int yCase, int numeroJoueur) {
+        int[][] directions = {
             { 1, 0 },
             { 0, 1 }, 
-            { 1, 1 }, 
-            { 1, -1 } 
+            { 1, 1 },
+            { 1, -1 }  
         };
-
-        for (int[] direction : directions) { 
-            int compteur = 1; 
-            
+    
+        for (int[] direction : directions) {
+            int compteur = 1;
             int x = xCase + direction[0];
             int y = yCase + direction[1];
-            while (x >= 0 && x < TAILLE_TABLEAU && y >= 0 && y < TAILLE_TABLEAU 
-                   && grille[x][y] == numeroJoueur && grilleSequences[x][y] == 0) {
+            while (x >= 0 && x < TAILLE_TABLEAU && y >= 0 && y < TAILLE_TABLEAU
+                   && grille[x][y] == numeroJoueur) {
                 compteur++;
                 x += direction[0];
                 y += direction[1];
             }
-            
             x = xCase - direction[0];
             y = yCase - direction[1];
-            while (x >= 0 && x < TAILLE_TABLEAU && y >= 0 && y < TAILLE_TABLEAU 
-                   && grille[x][y] == numeroJoueur && grilleSequences[x][y] == 0) {
+            while (x >= 0 && x < TAILLE_TABLEAU && y >= 0 && y < TAILLE_TABLEAU
+                   && grille[x][y] == numeroJoueur) {
                 compteur++;
+
                 x -= direction[0];
                 y -= direction[1];
             }
-
-            if (compteur >= LONGUEUR_LIGNE) {
+    
+            if (compteur == LONGUEUR_LIGNE) {
                 System.out.println("Ligne détectée à partir de la case (" + xCase + ", " + yCase + ")");
+                grillePrecedente = grille;
+                return true;
             }
         }
-        grilleSequences[xCase][yCase] = numeroJoueur;
+        grillePrecedente = grille;
+        return false;
     }
-    
-    
+
+
     @Override
     public String toString() {
-        String resultat = "";
+        StringBuilder resultat = new StringBuilder("Grille :\n");
         for (int i = 0; i < grille.length; i++) {
             for (int j = 0; j < grille[i].length; j++) {
-                resultat += grille[i][j] + " ";
+                resultat.append(grille[i][j]).append(" ");
             }
-            resultat += "\n";
+            resultat.append("\n");
         }
-        return resultat; 
+        resultat.append("\nGrillePrecedente :\n");
+        for (int i = 0; i < grillePrecedente.length; i++) {
+            for (int j = 0; j < grillePrecedente[i].length; j++) {
+                resultat.append(grillePrecedente[i][j]).append(" ");
+            }
+            resultat.append("\n");
+        }
+        return resultat.toString();
     }
 }
